4ba6b2a2a6a86895395a9d28a1b3cce70f9ffe59
vanshajkapoor <36599976+vanshajkapoor@users.noreply.github.com>
2020-01-09 18:25:59 +0530

    added users router and controller

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const homeController =require('../controllers/home_controller');
 
 
 router.get('/',homeController.home);
+//any further routes will be required below
+router.use('/users',require('./users'));//when /user/xyz comes in it will require users.js and map to corresponding controller
 console.log("Router Loaded");
 module.exports=router;
